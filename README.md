aceddd7f177b23c35060ff5812194fc3b5179553
Sophia Li <sophiali124@gmail.com>
2020-03-17 21:44:22 -0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# sourdough-timer
-Timer to feed sourdough starter. 
+<h1 align="center">Sourdough Timer</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://twitter.com/sophia_wyl">
+    <img alt="Twitter: sophia_wyl" src="https://img.shields.io/twitter/follow/sophia_wyl.svg?style=social" target="_blank" />
+  </a>
+</p>
+
+> Timer to feed sourdough starter.
+
+## Technologies
+
+- Microsoft Azure Functions
+- Twilio
+- Node.js
+
+## Author
+
+👤 Sophia
+
+- Twitter: [@sophia_wyl](https://twitter.com/sophia_wyl)
+- GitHub: [@sophi-li](https://github.com/sophi-li)
---
+++ SourdoughTimerTrigger/index.js
@@ -0,0 +1,38 @@
+// Save Twilio Keys
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+module.exports = async function(context, myTimer) {
+  var timeStamp = new Date().toISOString();
+
+  if (myTimer.isPastDue) {
+    context.log("JavaScript is running late!");
+  }
+  context.log("JavaScript timer trigger function ran!", timeStamp);
+
+  // Text message reminder set up
+  client.messages
+    .create({
+      from: process.env.SENDER_NUMBER,
+      body: "Don't forget to feed me!",
+      to: process.env.RECIPIENT_NUMBER
+    })
+    .then(message => {
+      context.log("Message sent");
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Text successfully sent"
+      };
+      context.log("JavaScript timer trigger function ran!", timeStamp);
+      context.done();
+    })
+    .catch(err => {
+      context.log.error("Twilio Error: " + err.message + " -- " + err.code);
+      context.res = {
+        status: 500,
+        body: `Twilio Error Message: ${err.message}\nTwilio Error code: ${err.code}`
+      };
+      context.done();
+    });
+};
